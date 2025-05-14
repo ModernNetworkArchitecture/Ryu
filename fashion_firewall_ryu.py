0e16f3c8b41e54549cc64e3129895a2b71ce02a0
syifani adillah salsabila <148206758+syifaniads@users.noreply.github.com>
2025-05-14 11:48:39 +0700

    Buat fashion_firewall_ryu.py

---
+++ fashion_firewall_ryu.py
@@ -0,0 +1,91 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet, ipv4
+
+class FashionFirewallSwitch(app_manager.RyuApp):
+    OFP_VERSION = [ofproto_v1_3.OFP_VERSION]
+
+    # IP rules: blokir langsung komunikasi antara UMKM tertentu
+    blocked_pairs = [
+        ('10.0.0.4', '10.0.0.1'),  # ResellerJakarta -> BatikJogja
+        ('10.0.0.1', '10.0.0.4')   # BatikJogja -> ResellerJakarta
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super(FashionFirewallSwitch, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Default rule: send to controller
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.mac_to_port[dpid][src] = in_port
+
+        out_port = self.mac_to_port[dpid].get(dst, ofproto.OFPP_FLOOD)
+
+        # Cek apakah ada IPv4 di paket
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+        if ip_pkt:
+            src_ip = ip_pkt.src
+            dst_ip = ip_pkt.dst
+            if (src_ip, dst_ip) in self.blocked_pairs:
+                self.logger.info("Blocked %s -> %s", src_ip, dst_ip)
+                return  # drop packet
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        match = parser.OFPMatch(in_port=in_port, eth_src=src, eth_dst=dst)
+
+        if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+            return
+        else:
+            self.add_flow(datapath, 1, match, actions)
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions,
+                                  data=msg.data)
+        datapath.send_msg(out)
